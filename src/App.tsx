f36f650ad58a333b5136f3e438f11eb6f30b5dac
Mizzade <bleiwolle@gmail.com>
2025-09-02 12:01:23 +0200

    feat: Taking turns

--- src/App.tsx
+++ src/App.tsx
@@ -19,12 +19,15 @@ export function Square() {
 
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(""));
 
   function handleClick(i) {
+    if (squares[i]) return;
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    nextSquares[i] = xIsNext ? "X" : "O";
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
   return (
     <>
