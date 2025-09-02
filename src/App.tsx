20fc6481fb4edcf2e11bb11e36cf344c1d0a36a3
Mizzade <bleiwolle@gmail.com>
2025-09-02 11:05:14 +0200

    feat: making an interactive component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
-export function Square({ value, onSquareClick }) {
+export function Square() {
+  const [value, setValue] = useState("");
+
+  function handleClick() {
+    setValue('X');
+  }
+
   return (
     <button
       className="square"
@@ -23,19 +29,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   )
