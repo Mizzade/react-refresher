08d949bfe5a1577fdcfc27bc001122282bbd890f
Mirko Lauff <bleiwolle@gmail.com>
2025-10-03 11:33:49 +0200

    feat: Hightlight winning squares (#15)

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import { useState } from "react";
 interface SquareProps {
   value: string;
   onSquareClick: () => void;
+  isWinningSquare: boolean;
 }
 
-export function Square({ value, onSquareClick }: SquareProps) {
+export function Square({ value, onSquareClick, isWinningSquare }: SquareProps) {
+  const className = isWinningSquare
+    ? "square winning"
+    : "square";
+
   return (
     <button
-      className="square"
+      className={className}
       onClick={onSquareClick}
     >
       {value}
@@ -33,14 +38,23 @@ export function Board({ xIsNext, squares, onPlay }: BoardProps) {
   const winner = calculateWinner(squares);
   const status = winner
     ? "Winner: " + winner
-    : "Next player: " + (xIsNext ? "X" : "O");
+    : checkIfDraw(squares)
+      ? "It's a draw!"
+      : "Next player: " + (xIsNext ? "X" : "O");
+
+  const winningSquares = calculateWinnerSquares(squares);
 
   const NUM_ROWS = 3;
   const NUM_COLS = 3;
   const board_squares = Array.from({ length: NUM_ROWS }, (_, rowIndex) => (
     <div key={rowIndex} className="board-row">
       {Array.from({ length: NUM_COLS }, (_, colIndex) => (
-        <Square value={squares[rowIndex * NUM_COLS + colIndex]} onSquareClick={() => handleClick(rowIndex * NUM_COLS + colIndex)} />
+        <Square
+          key={rowIndex * NUM_COLS + colIndex}
+          value={squares[rowIndex * NUM_COLS + colIndex]}
+          onSquareClick={() => handleClick(rowIndex * NUM_COLS + colIndex)}
+          isWinningSquare={winningSquares.includes(rowIndex * NUM_COLS + colIndex)}
+        />
       ))}
     </div>
   ))
@@ -104,8 +118,11 @@ export default function Game() {
   )
 }
 
+function checkIfDraw(squares: string []) {
+  return squares.every(square => square !== "") && !calculateWinner(squares);
+}
 
-function calculateWinner(squares: string[]) {
+function calculateWinnerSquares(squares: string[]) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -119,10 +136,17 @@ function calculateWinner(squares: string[]) {
 
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a] === squares[b] && squares[b] == squares[c]) {
-      return squares[a];
+    if (squares[a] === squares[b] && squares[b] == squares[c] && squares[a]) {
+      return lines[i];
     }
   }
 
-  return null
+  return []
+}
+
+function calculateWinner(squares: string[]) {
+  const result = calculateWinnerSquares(squares);
+  return result.length
+    ? squares[result[0]]
+    : null;
 }
