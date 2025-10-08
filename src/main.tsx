f6fee9dfdb58b6446f222e8cccd381b75d34123e
Mizzade <bleiwolle@gmail.com>
2025-10-08 16:01:50 +0200

    Import/Export Gallery

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
+import Gallery, { Profile } from './Gallery.tsx'
 import TicTacToe from './TicTacToe.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Gallery />
+    <Profile />
     <TicTacToe />
   </StrictMode>,
 )
