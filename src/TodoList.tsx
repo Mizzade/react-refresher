695a069b7ceb81bb39267f1cd4ab9d8564f7e342
Mizzade <bleiwolle@gmail.com>
2025-10-09 13:19:28 +0200

    Add Js in Jsx

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,17 +1,26 @@
+const person = {
+  name: "Gregorio Y. Zara",
+  imageUrl: "https://i.imgur.com/7vQD0fPs.jpg",
+  theme: {
+    backgroundColor: "black",
+    color: "pink"
+  }
+}
+
 export default function TodoList() {
   return (
-    <>
-      <h1>Hedy Lamarr's Todos</h1>
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
       <img
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Hedy Lamarr"
-        className="photo"
+        src={person.imageUrl}
+        alt={person.name}
+        className="avatar"
       />
       <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve the spectrum technology</li>
+         <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
       </ul>
-    </>
+    </div>
   );
 }
