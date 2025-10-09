33c8684fc78ce80b776f55c53d36177bf8d6f530
Mizzade <bleiwolle@gmail.com>
2025-10-09 14:13:10 +0200

    Pass props

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,34 @@ import './index.css'
 import Gallery, { Profile } from './Gallery.tsx'
 import TicTacToe from './TicTacToe.tsx'
 import TodoList from './TodoList.tsx'
+import { Avatar } from './Avatar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <TodoList />
     <Gallery />
     <Profile />
+    <Avatar
+      size={100}
+      person={{
+        name: 'Katsuko Saruhashi',
+        imageId: 'YfeOqp2'
+      }}
+      />
+    <Avatar
+        size={80}
+        person={{
+          name: 'Aklilu Lemma',
+          imageId: 'OKS67lh'
+        }}
+      />
+      <Avatar
+        size={50}
+        person={{
+          name: 'Lin Lanying',
+          imageId: '1bX5QH6'
+        }}
+      />
     <TicTacToe />
   </StrictMode>,
 )
