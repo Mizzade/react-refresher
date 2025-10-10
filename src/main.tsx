3b2f70172ee27eb5672bc746f59833a25a109fb9
Mirko Lauff <bleiwolle@gmail.com>
2025-10-10 10:40:25 +0200

    Add Js in Jsx (#22)

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Gallery, { Profile } from './Gallery.tsx'
 import TicTacToe from './TicTacToe.tsx'
+import TodoList from './TodoList.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <TodoList />
     <Gallery />
     <Profile />
     <TicTacToe />
