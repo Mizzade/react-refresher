627a77cf516565ddb18b150bf1447da05bcdd568
Mirko Lauff <bleiwolle@gmail.com>
2025-10-13 14:04:00 +0200

    Tutorial/describing the UI (#25)
    * Move tic-tac-toe game to own module
    
    * Tutorial/your first component (#19)
    
    * Define a component
    * Import into main module
    
    * Import/Export Gallery (#20)
    
    * Add TodoList (#21)
    
    * Add Js in Jsx (#22)
    
    * tutorial/passing props to components (#23)
    
    * Pass props
    
    * Add list of scientists (#24)

---
+++ src/Avatar.tsx
@@ -0,0 +1,14 @@
+import { getImageUrl } from './utils';
+
+
+export function Avatar({ person, size = 100 }) {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
+}
