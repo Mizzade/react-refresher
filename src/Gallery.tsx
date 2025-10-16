976391f61bbb1aa13cad0929e1353153f7e2c2e0
Mizzade <bleiwolle@gmail.com>
2025-10-16 13:26:46 +0200

    Add sculpture gallery

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react';
+import { sculptureList } from './data/sculptures';
 export function Profile() {
   return (
     <img
       src="https://i.imgur.com/MK3eW3Am.jpg"
       alt="Katherine Johnson"
-      />
+    />
   )
 }
 
@@ -15,5 +17,58 @@ export default function Gallery() {
       <Profile />
       <Profile />
     </section>
+  )
+}
+
+export function SculptureGallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleNextClick() {
+    const numItems = sculptureList.length;
+    setIndex((index + 1) % numItems);
+  }
+
+  function handlePreviousClick() {
+    const numItems = sculptureList.length;
+    setIndex((index + numItems - 1) % numItems);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  const sculpture = sculptureList[index];
+
+  return (
+    <>
+      <button onClick={handlePreviousClick}>
+        Prevous
+      </button>
+      <button onClick={handleNextClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button
+        style={{ display: 'block' }}
+        onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img
+        src={sculpture.url}
+        alt={sculpture.alt}
+      />
+      <p>
+        {sculpture.description}
+      </p>
+
+    </>
   )
 }
--- src/List.tsx
+++ src/List.tsx
@@ -1,4 +1,4 @@
-import { people } from './data.js'
+import { people } from './data/persons.js'
 import { getImageUrl } from './utils';
 
 export default function List() {
